5343641b7e0daaf63f574868435dd3e0eeaa2790
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2019-05-03 10:11:35 +0200

    Initial code drop

---
+++ Blippit/Blippit.h
@@ -0,0 +1,19 @@
+//
+//  Blippit.h
+//  Blippit
+//
+//  Created by Jerson Perpetua on 2019-05-03.
+//  Copyright © 2019 Crunchfish AB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Blippit.
+FOUNDATION_EXPORT double BlippitVersionNumber;
+
+//! Project version string for Blippit.
+FOUNDATION_EXPORT const unsigned char BlippitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Blippit/PublicHeader.h>
+
+
