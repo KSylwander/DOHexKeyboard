005db5e4eed575d4493d30e2d12ba6ad46cc2ad7
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2019-05-06 10:26:23 +0200

    Implement initial cancellable artifact

---
+++ Blippit/Commons/Cancellable.swift
@@ -0,0 +1,12 @@
+//
+//  Cancellable.swift
+//  Blippit
+//
+//  Created by Jerson Perpetua on 2019-05-06.
+//  Copyright © 2019 Crunchfish AB. All rights reserved.
+//
+
+/* Conforming artifacts can be cancelled */
+protocol Cancellable {
+  func cancel()
+}
