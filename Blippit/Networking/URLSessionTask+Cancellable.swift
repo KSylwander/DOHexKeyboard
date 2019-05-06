5ff2e2816fdeb879caabdb95d5ee347c87402179
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2019-05-06 10:39:20 +0200

    Make the URL session task artifact adhere to the cancellable protocol

---
+++ Blippit/Networking/URLSessionTask+Cancellable.swift
@@ -0,0 +1,11 @@
+//
+//  URLSessionTask+Cancellable.swift
+//  Blippit
+//
+//  Created by Jerson Perpetua on 2019-05-06.
+//  Copyright © 2019 Crunchfish AB. All rights reserved.
+//
+
+import Foundation
+
+extension URLSessionTask: Cancellable {}
