90e99458d5ca24446d3fc687813683795ce1baf9
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2019-05-07 13:47:20 +0200

    Implement initial raw state artifact

---
+++ Blippit/State Handling/RawState.swift
@@ -0,0 +1,15 @@
+//
+//  RawState.swift
+//  Blippit
+//
+//  Created by Jerson Perpetua on 2019-05-07.
+//  Copyright © 2019 Crunchfish AB. All rights reserved.
+//
+
+import Foundation
+
+/* State representations that include relevant metadata to create the corresponding actual states */
+enum RawState {
+  case initial
+  case started
+}
