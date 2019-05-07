63c99f306805dc8b2ca10b3784973b0c283820d8
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2019-05-07 13:51:51 +0200

    Implement initial Blippit error artifact

---
+++ Blippit/Commons/BlippitError.swift
@@ -0,0 +1,23 @@
+//
+//  BlippitError.swift
+//  Blippit
+//
+//  Created by Jerson Perpetua on 2019-05-07.
+//  Copyright © 2019 Crunchfish AB. All rights reserved.
+//
+
+import Podz
+
+enum BlippitError {
+  case invalidPodzStatus(
+    /* 1. `PodzStatus.pending(PodzError)`: `PodzError` represents a user-resolvable error (i.e., should be handled in a
+     *    user-friendly way). Each error is passed in the pending state after the previous one is resolved. The payment
+     *    controller will resume on its own after all errors are taken care of.
+     * 2. `PodzStatus.locked`: The API key and/or app ID cannot be verified. The payment controller has to be recreated
+     *    after the error is resolved.
+     */
+    PodzStatus
+  )
+}
+
+extension BlippitError: Error {}
