024333dfc539e3a8442f0bc4925322352fe3b69e
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2019-05-08 14:56:25 +0200

    Generate mock for the pod artifact

---
+++ BlippitTests/Mocks/DummyPod.swift
@@ -0,0 +1,19 @@
+//
+//  DummyPod.swift
+//  BlippitTests
+//
+//  Created by Jerson Perpetua on 2019-05-08.
+//  Copyright © 2019 Crunchfish AB. All rights reserved.
+//
+
+import Podz
+
+/* Allows `Pod`to be mocked from within this project. See `Mocks.generated.swift` for the generated mock. */
+protocol DummyPod: Pod {
+  var pid: UInt32 { get }
+
+  var state: PodState { get }
+
+  var onStateChanged: ((Pod, PodState) -> Void)? { get set }
+  var onUpdated: ((Pod, Date, Int, Int, Int) -> Void)? { get set }
+}
