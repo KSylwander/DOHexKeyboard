e55ac525919d3c2232cd43edee79f75247c8bfb0
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2019-05-08 14:52:14 +0200

    Implement initial constants artifact

---
+++ Blippit/Commons/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Blippit
+//
+//  Created by Jerson Perpetua on 2019-05-08.
+//  Copyright © 2019 Crunchfish AB. All rights reserved.
+//
+
+import os.log
+
+private final class Placeholder: NSObject {}
+
+enum Constants {
+  static let log = OSLog(
+    subsystem: Bundle(for: Placeholder.self).bundleIdentifier!,
+    // TODO: Replace placeholder with Blippit artifact
+    category: String(describing: Placeholder.self)
+  )
+}
