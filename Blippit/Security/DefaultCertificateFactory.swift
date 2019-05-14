121d8aacb459528b4eb6f38c97f633e0f878419d
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2019-05-14 13:48:34 +0200

    Implement initial default certificate factory

---
+++ Blippit/Security/DefaultCertificateFactory.swift
@@ -0,0 +1,23 @@
+//
+//  DefaultCertificateFactory.swift
+//  Blippit
+//
+//  Created by Jerson Perpetua on 2019-05-13.
+//  Copyright © 2019 Crunchfish AB. All rights reserved.
+//
+
+import Foundation
+
+struct DefaultCertificateFactory {
+  let certificateHandling: CertificateHandling
+}
+
+extension DefaultCertificateFactory: CertificateFactory {
+  func makeCertificate(url: URL) throws -> Certificate {
+    let data = try Data(contentsOf: url)
+    guard let certificate = certificateHandling.createWithData(nil, data as CFData) else {
+      throw SecurityError.invalidCertificateData
+    }
+    return certificate
+  }
+}
