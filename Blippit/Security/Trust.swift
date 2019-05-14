1db3fffcd275e861a1f510ba41f3e66543d0b41c
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2019-05-14 13:16:03 +0200

    Implement initial trust artifact

---
+++ Blippit/Security/Trust.swift
@@ -0,0 +1,12 @@
+//
+//  Trust.swift
+//  Blippit
+//
+//  Created by Jerson Perpetua on 2019-05-14.
+//  Copyright © 2019 Crunchfish AB. All rights reserved.
+//
+
+import Foundation
+
+protocol Trust {}
+extension SecTrust: Trust {}
