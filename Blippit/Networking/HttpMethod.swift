0e847721750b8dd4801fdd1b7571960e3ce03230
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2019-05-14 15:33:13 +0200

    Implement initial HTTP method artifact

---
+++ Blippit/Networking/HttpMethod.swift
@@ -0,0 +1,17 @@
+//
+//  HttpMethod.swift
+//  Blippit
+//
+//  Created by Jerson Perpetua on 2019-05-14.
+//  Copyright © 2019 Crunchfish AB. All rights reserved.
+//
+
+import Foundation
+
+enum HttpMethod: String {
+  case get = "GET"
+  case post = "POST"
+  case put = "PUT"
+  case delete = "DELETE"
+  case patch = "PATCH"
+}
