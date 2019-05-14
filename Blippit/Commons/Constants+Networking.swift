6ca2e9a0b269bd7f7af7b15986bfee809c27d8de
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2019-05-14 15:32:39 +0200

    Implement initial networking constants artifact

---
+++ Blippit/Commons/Constants+Networking.swift
@@ -0,0 +1,21 @@
+//
+//  Constants+Networking.swift
+//  Blippit
+//
+//  Created by Jerson Perpetua on 2019-05-14.
+//  Copyright © 2019 Crunchfish AB. All rights reserved.
+//
+
+import Foundation
+
+extension Constants {
+  enum api {
+    enum defaults {
+      static let basePath = ""
+      static let apiKey = ""
+
+      static let cachePolicy = URLRequest.CachePolicy.useProtocolCachePolicy
+      static let timeoutInterval = TimeInterval(60.0)
+    }
+  }
+}
