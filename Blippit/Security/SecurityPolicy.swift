8cfe48f40fb2128dc96baa62058d4c69d40968fe
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2019-05-14 13:14:40 +0200

    Implement initial security policy artifact

---
+++ Blippit/Security/SecurityPolicy.swift
@@ -0,0 +1,12 @@
+//
+//  SecurityPolicy.swift
+//  Blippit
+//
+//  Created by Jerson Perpetua on 2019-05-14.
+//  Copyright © 2019 Crunchfish AB. All rights reserved.
+//
+
+import Foundation
+
+protocol SecurityPolicy {}
+extension SecPolicy: SecurityPolicy {}
