3e97fb13eed7347991fa2fb4ce3ed157dbb6ddc0
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2019-05-14 13:15:16 +0200

    Implement initial certificate artifact

---
+++ Blippit/Security/Certificate.swift
@@ -0,0 +1,12 @@
+//
+//  Certificate.swift
+//  Blippit
+//
+//  Created by Jerson Perpetua on 2019-05-14.
+//  Copyright © 2019 Crunchfish AB. All rights reserved.
+//
+
+import Foundation
+
+protocol Certificate {}
+extension SecCertificate: Certificate {}
