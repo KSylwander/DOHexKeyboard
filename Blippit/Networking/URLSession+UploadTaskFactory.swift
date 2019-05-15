fa4935dfdd725061e5e645925d484c75f34dc62c
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2019-05-15 11:12:57 +0200

    Make the URL session artifact adhere to the upload task factory protocol

---
+++ Blippit/Networking/URLSession+UploadTaskFactory.swift
@@ -0,0 +1,11 @@
+//
+//  URLSession+UploadTaskFactory.swift
+//  Blippit
+//
+//  Created by Jerson Perpetua on 2019-05-15.
+//  Copyright © 2019 Crunchfish AB. All rights reserved.
+//
+
+import Foundation
+
+extension URLSession: UploadTaskFactory {}
