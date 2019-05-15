7f06a776d27aa60f0524e37b578d8ef978b88a7c
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2019-05-15 11:14:32 +0200

    Implement initial default upload data use case

---
+++ Blippit/Networking/DefaultUploadDataUseCase.swift
@@ -0,0 +1,34 @@
+//
+//  DefaultUploadDataUseCase.swift
+//  Blippit
+//
+//  Created by Jerson Perpetua on 2019-05-15.
+//  Copyright © 2019 Crunchfish AB. All rights reserved.
+//
+
+import Foundation
+
+struct DefaultUploadDataUseCase {
+  let uploadTaskFactory: UploadTaskFactory
+}
+
+extension DefaultUploadDataUseCase: UploadDataUseCase {
+  @discardableResult func uploadData(with request: URLRequest,
+                                     from data: Data,
+                                     completion: @escaping Completion) -> Cancellable {
+
+    let task = uploadTaskFactory.uploadTask(with: request, from: data) { data, response, error in
+      let response = response.map { $0 as! HTTPURLResponse }
+
+      if let error = error {
+        completion(response, .failure(error))
+        return
+      }
+
+      completion(response, .success(data!))
+    }
+    task.resume()
+
+    return task
+  }
+}
