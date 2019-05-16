f178d2d429aa32c4ac13d9b923deb48ae4d918cb
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2019-05-16 15:33:25 +0200

    Implement initial setup transfer ID state factory

---
+++ Blippit/State Handling/SetupTransferIdStateFactory.swift
@@ -0,0 +1,13 @@
+//
+//  SetupTransferIdStateFactory.swift
+//  Blippit
+//
+//  Created by Jerson Perpetua on 2019-05-16.
+//  Copyright © 2019 Crunchfish AB. All rights reserved.
+//
+
+import Podz
+
+protocol SetupTransferIdStateFactory {
+  func makeState(delegate: StateDelegate, pid: UInt32, session: PodSession) -> State
+}
