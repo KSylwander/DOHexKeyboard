2d56fc4a999211abcc41519d01c74421e3b4020a
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2019-05-16 13:37:31 +0200

    Implement initial default HTTP status code validator

--- Blippit/Commons/BlippitError.swift
+++ Blippit/Commons/BlippitError.swift
@@ -21,6 +21,8 @@ enum BlippitError {
   case invalidPodSessionState(PodSessionState)
   case podDoesNotTransferId
 
+  case invalidHttpStatusCode(Int)
+
   /* Sending of data token failed after `Constants.transactionStage.maxRetries + 1` attempts. May be fixed by asking
    * the user to blip again.
    */
