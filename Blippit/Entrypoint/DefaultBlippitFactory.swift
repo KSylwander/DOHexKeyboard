8ebb1953270c06026c7ee8a2647af8fd13ac46f1
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2019-05-16 11:01:41 +0200

    Implement initial default Blippit factory

---
+++ Blippit/Entrypoint/DefaultBlippitFactory.swift
@@ -0,0 +1,27 @@
+//
+//  DefaultBlippitFactory.swift
+//  Blippit
+//
+//  Created by Jerson Perpetua on 2019-05-16.
+//  Copyright © 2019 Crunchfish AB. All rights reserved.
+//
+
+import Podz
+
+struct DefaultBlippitFactory {
+  let establishCloudSessionUseCase: EstablishCloudSessionUseCase
+  let uploadCommandDataUseCase: UploadCommandDataUseCase
+  let retryHandlerFactory: RetryHandlerFactory
+}
+
+extension DefaultBlippitFactory: BlippitFactory {
+  func makeBlippit(delegate: BlippitDelegate, configuration: BlippitConfiguration) throws -> Blippit {
+    return DefaultBlippit(
+      delegate: delegate,
+      podz: try PodzSetup.setup(appId: configuration.podzAppId, apiKey: configuration.podzApiKey),
+      establishCloudSessionUseCase: establishCloudSessionUseCase,
+      uploadCommandDataUseCase: uploadCommandDataUseCase,
+      retryHandlerFactory: retryHandlerFactory
+    )
+  }
+}
