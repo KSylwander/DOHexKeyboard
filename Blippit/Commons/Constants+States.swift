cf7070846994da867b28e0cf9cdf15ab29d63b4f
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2019-05-16 12:16:44 +0200

    Implement initial state constants artifacts

---
+++ Blippit/Commons/Constants+States.swift
@@ -0,0 +1,17 @@
+//
+//  Constants+States.swift
+//  Blippit
+//
+//  Created by Jerson Perpetua on 2019-05-16.
+//  Copyright © 2019 Crunchfish AB. All rights reserved.
+//
+
+import Foundation
+
+extension Constants {
+  enum states {
+    enum transferDataToken {
+      static let maxRetries = 3
+    }
+  }
+}
