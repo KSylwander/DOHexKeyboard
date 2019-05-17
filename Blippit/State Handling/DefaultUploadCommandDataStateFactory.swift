bd60e6a3ac74834fd3e044c1578cea40c1961a52
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2019-05-17 08:58:45 +0200

    Implement initial default upload command data state factory

---
+++ Blippit/State Handling/DefaultUploadCommandDataStateFactory.swift
@@ -0,0 +1,25 @@
+//
+//  DefaultUploadCommandDataStateFactory.swift
+//  Blippit
+//
+//  Created by Jerson Perpetua on 2019-05-16.
+//  Copyright © 2019 Crunchfish AB. All rights reserved.
+//
+
+import Podz
+
+struct DefaultUploadCommandDataStateFactory {
+  let uploadCommandDataUseCase: UploadCommandDataUseCase
+}
+
+extension DefaultUploadCommandDataStateFactory: UploadCommandDataStateFactory {
+  func makeState(delegate: StateDelegate, cloudSessionId: String, data: String, podSession: PodSession) -> State {
+    return UploadCommandDataState(
+      delegate: delegate,
+      cloudSessionId: cloudSessionId,
+      data: data,
+      podSession: podSession,
+      uploadCommandDataUseCase: uploadCommandDataUseCase
+    )
+  }
+}
