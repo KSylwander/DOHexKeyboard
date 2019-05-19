f360e91b3fd630734ce4b7402a8f77983d392705
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2019-05-19 22:18:39 +0200

    Implement initial default establish cloud session state factory

---
+++ Blippit/State Handling/DefaultEstablishCloudSessionStateFactory.swift
@@ -0,0 +1,25 @@
+//
+//  DefaultEstablishCloudSessionStateFactory.swift
+//  Blippit
+//
+//  Created by Jerson Perpetua on 2019-05-17.
+//  Copyright © 2019 Crunchfish AB. All rights reserved.
+//
+
+import Podz
+
+struct DefaultEstablishCloudSessionStateFactory {
+  let establishCloudSessionUseCase: EstablishCloudSessionUseCase
+}
+
+extension DefaultEstablishCloudSessionStateFactory: EstablishCloudSessionStateFactory {
+  func makeState(delegate: StateDelegate, pid: UInt32, userId: String, podSession: PodSession) -> State {
+    return EstablishCloudSessionState(
+      delegate: delegate,
+      pid: pid,
+      userId: userId,
+      podSession: podSession,
+      establishCloudSessionUseCase: establishCloudSessionUseCase
+    )
+  }
+}
