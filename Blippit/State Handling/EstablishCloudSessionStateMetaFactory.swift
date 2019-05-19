756f7a4f5362b6357de169e66c7213fa876fbdcb
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2019-05-19 22:19:29 +0200

    Implement initial establish cloud session state meta factory

---
+++ Blippit/State Handling/EstablishCloudSessionStateMetaFactory.swift
@@ -0,0 +1,13 @@
+//
+//  EstablishCloudSessionStateMetaFactory.swift
+//  Blippit
+//
+//  Created by Jerson Perpetua on 2019-05-19.
+//  Copyright © 2019 Crunchfish AB. All rights reserved.
+//
+
+import Foundation
+
+protocol EstablishCloudSessionStateMetaFactory {
+  func makeFactory(appId: UUID) -> EstablishCloudSessionStateFactory
+}
