ce32af448aad85501369f00ae0c99eb40f5d7e94
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2019-05-24 11:10:57 +0200

    Add initial BlippitTest app target

---
+++ BlippitTest/Entrypoint/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  BlippitTest
+//
+//  Created by Jerson Perpetua on 2019-05-24.
+//  Copyright © 2019 Crunchfish AB. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder {
+  var window: UIWindow?
+}
+
+extension AppDelegate: UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+    // TODO: Implement me
+    return true
+  }
+}
