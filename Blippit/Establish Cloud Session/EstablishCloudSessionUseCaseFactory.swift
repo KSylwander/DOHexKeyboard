68dcab1e2a1ef014000094e72c9ccdce03db5dc6
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2019-05-27 08:57:42 +0200

    Implement initial establish cloud session use case factory

---
+++ Blippit/Establish Cloud Session/EstablishCloudSessionUseCaseFactory.swift
@@ -0,0 +1,13 @@
+//
+//  EstablishCloudSessionUseCaseFactory.swift
+//  Blippit
+//
+//  Created by Jerson Perpetua on 2019-05-16.
+//  Copyright © 2019 Crunchfish AB. All rights reserved.
+//
+
+import Foundation
+
+protocol EstablishCloudSessionUseCaseFactory {
+  func makeUseCase(appId: UUID) -> EstablishCloudSessionUseCase
+}
