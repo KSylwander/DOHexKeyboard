6161f8e2987fadc39cecd170fa78973f9735c65c
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2019-10-17 14:29:18 +0200

    Implement initial transfer payer ID state factory

---
+++ Blippit/Construction Root/DefaultTransferPayerIdStateFactory.swift
@@ -0,0 +1,25 @@
+//
+//  DefaultTransferPayerIdStateFactory.swift
+//  Blippit
+//
+//  Created by Jerson Perpetua on 2019-10-17.
+//  Copyright © 2019 Crunchfish AB. All rights reserved.
+//
+
+import Podz
+
+struct DefaultTransferPayerIdStateFactory {
+  let retryHandlerFactory: RetryHandlerFactory
+  let payerId: TransferId
+}
+
+extension DefaultTransferPayerIdStateFactory: TransferPayerIdStateFactory {
+  func makeState(delegate: StateDelegate, session: PodSession) -> State {
+    return TransferPayerIdState(
+      delegate: delegate,
+      session: session,
+      payerId: payerId,
+      retryHandlerFactory: retryHandlerFactory
+    )
+  }
+}
