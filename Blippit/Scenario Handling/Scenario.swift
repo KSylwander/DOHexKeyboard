fa501255bcc01fc51530348d0d083eb02e699678
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2020-01-10 07:16:29 +0100

    Simplify scenario conformance requirements

--- Blippit/Scenario Handling/Scenario.swift
+++ Blippit/Scenario Handling/Scenario.swift
@@ -12,34 +12,3 @@ protocol Scenario {
   func start()
   func stop()
 }
-
-protocol TransitionStateFactory {
-  func makeStartState() -> State
-  func makeStopState() -> State?
-
-  func makeState(for transition: StateTransition) -> State
-}
-
-extension Scenario where Self: TransitionStateFactory {
-  func start() {
-    delegate?.scenario(self, moveTo: makeStartState())
-  }
-
-  func stop() {
-    delegate?.scenario(self, moveTo: makeStopState())
-  }
-}
-
-extension Scenario where Self: StateDelegate, Self: TransitionStateFactory {
-  func move(to transition: StateTransition) {
-    guard let delegate = delegate else {
-      return
-    }
-    let nextState = makeState(for: transition)
-    delegate.scenario(self, moveTo: nextState)
-  }
-
-  func state(_ state: State, didFailWithError error: Error) {
-    delegate?.scenario(self, didFailWithError: error)
-  }
-}
