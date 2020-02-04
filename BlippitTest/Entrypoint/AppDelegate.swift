079b6adeca6b90ad6bd3c9dde87e1ab23bf314eb
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2020-02-04 16:16:06 +0100

    Remove unneeded API key and app ID configuration from the BlippitTest app

--- BlippitTest/Entrypoint/AppDelegate.swift
+++ BlippitTest/Entrypoint/AppDelegate.swift
@@ -18,20 +18,9 @@ extension AppDelegate: UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-#if PRODUCTION
-    let apiKey = UUID(uuidString: "66A69E05-3750-41FE-8C08-8A57D9F7CBAD")!
-#elseif STAGE
-    let apiKey = UUID(uuidString: "a6d9a801-752b-433f-b0e9-19b7c290afcd")!
-#else
-    let apiKey = UUID(uuidString: "d48a1a59-8b39-4a83-8355-fb2712b2d53f")!
-#endif
-
     let window = self.window!
     window.rootViewController = MainViewController.instantiate(
-      blippitFactory: DefaultBlippitFactory(
-        apiKey: apiKey,
-        appId: UUID(uuidString: "CF000000-0000-0000-0000-000000000004")!
-      )
+      blippitFactory: DefaultBlippitFactory()
     )
     window.makeKeyAndVisible()
 
