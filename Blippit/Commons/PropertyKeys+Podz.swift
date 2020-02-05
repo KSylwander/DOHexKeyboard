13221474d2c4f9e89631534afdca976c47276009
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2020-02-05 12:19:46 +0100

    Replace constant access to the Podz channel with property access

---
+++ Blippit/Commons/PropertyKeys+Podz.swift
@@ -0,0 +1,21 @@
+////
+//  PropertyKeys+Podz
+//  BlippitKit
+//
+//  Copyright © 2020 Crunchfish Proximity AB. All rights reserved.
+//
+
+extension PropertyKeys {
+  enum Podz {
+    enum Channel: PropertyKey {
+      static let rawValue = "Podz.Channel"
+
+#if DEBUG
+      static let defaultValue = "00000004"
+#else
+      static let defaultValue = "c562f73f"
+#endif
+    }
+    static let channel = Channel.self
+  }
+}
