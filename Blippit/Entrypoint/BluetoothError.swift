bd359749664cf18ce2bfa8500ec294472f0ddc66
Jerson Michael Perpetua <jerson.perpetua@crunchfish.com>
2020-02-24 14:48:14 +0100

    Implement additional Bluetooth errors for iOS 13

--- Blippit/Entrypoint/BluetoothError.swift
+++ Blippit/Entrypoint/BluetoothError.swift
@@ -28,12 +28,33 @@ public enum BluetoothError {
   case poweredOff
 
   /**
-   * The app is not authorized to use the BLE peripheral/central role.
+   * The app is not authorized to use the BLE peripheral/central role. Can only happen for iOS versions less than 13.0.
    *
    * Receive these errors by implementing the `BlippitDelegate.blippit(_:didFailWithError:)` method, and passing the
    * delegate implementation to the `BlippitSetup.setup(delegate:onBlipPayload:)` method.
    */
   case unauthorized
+
+  /**
+   * The app is not authorized to use Bluetooth. Can only happen for iOS 13.0 and up.
+   *
+   * Receive these errors by implementing `BlippitDelegate.blippit(_:didFailWithError:)` method, and passing the
+   * delegate implementation to the `BlippitSetup.setup(delegate:onBlipPayload:)` method.
+   *
+   * The user can't change the Bluetooth authorization status when this error is received, possibly due to active
+   * restrictions such as parental controls.
+   */
+  case restricted
+
+  /**
+   * The user explicitly denied Bluetooth access for this app. Can only happen for iOS 13.0 and up.
+   *
+   * Receive these errors by implementing `BlippitDelegate.blippit(_:didFailWithError:)` method, and passing the
+   * delegate implementation to the `BlippitSetup.setup(delegate:onBlipPayload:)` method.
+   *
+   * Blippit will continue on its own after Bluetooth is granted access.
+   */
+  case denied
 }
 
 extension BluetoothError: Error {}
